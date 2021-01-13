13b1450e19dcd0ddfc05329d945c213aa2c4da93
Domenick Braccia <domenick.braccia@gmail.com>
2021-01-12 20:41:46 -0500

    starting transcriptomic plotting

---
+++ code/local/gene_expression.R
@@ -0,0 +1,23 @@
+# =========================== gene_expression.R ============================= #
+#' description: processing and plotting of gene expression data for curated
+#' cysteine degrading genes. Diamand (blastx mode) was used to map reads to 
+#' custom database of specific genes examined in this paper.
+#' 
+#' NOTE: it is assumed that all necessary packages and data are loaded into the 
+#' R session where this script is being run.
+# =========================================================================== #
+
+
+
+#### ======================== loading in data =========================== #####
+print("- loading data")
+read.csv("../../results/diamond_processed/")
+
+#### ==================================================================== #####
+
+
+
+#### ======================== processing data =========================== #####
+
+
+#### ==================================================================== #####
